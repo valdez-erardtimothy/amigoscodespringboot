a7e904def5ce9501f3d3670ab14497404cef8d11
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-06-27 16:46:14 +0800

    "A simple api with Spring Boot"

--- src/main/java/valdezet/amigoscodespringboot/AmigoscodespringbootApplication.java
+++ src/main/java/valdezet/amigoscodespringboot/AmigoscodespringbootApplication.java
@@ -2,12 +2,22 @@ package valdezet.amigoscodespringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AmigoscodespringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmigoscodespringbootApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World!");
 	}
 
 }
