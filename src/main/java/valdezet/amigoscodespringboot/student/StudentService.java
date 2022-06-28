31fcd475dd645f6dee1701aab5bca9ce0bba10b5
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-06-28 11:12:59 +0800

    "Exercise" chapter

--- src/main/java/valdezet/amigoscodespringboot/student/StudentService.java
+++ src/main/java/valdezet/amigoscodespringboot/student/StudentService.java
@@ -3,7 +3,9 @@ package valdezet.amigoscodespringboot.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -27,13 +29,27 @@ public class StudentService {
 
     public void deleteStudent(Long studentId) {
         boolean exists = studentRepository.existsById(studentId);
-        if(!exists) {
-            throw new IllegalArgumentException(String.format(
-                    "Student with id %d does not exist",
-                    studentId
-            ));
+        if (!exists) {
+            throw produceNotExistsExceptionObject(studentId);
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(
+            Long studentId,
+            String newName,
+            String newEmail
+    ) {
+
+        Student existingStudent = studentRepository.findById(studentId)
+                .orElseThrow(() -> produceNotExistsExceptionObject(studentId));
+        if (!newEmail.equals(existingStudent.getEmail())) {
+            ensureUniqueEmail(existingStudent);
+        }
+        existingStudent.setName(newName);
+        existingStudent.setEmail(newEmail);
+
     }
 
     /* validation methods */
@@ -42,17 +58,21 @@ public class StudentService {
      * checks a new Student's (not yet saved) email
      * if it already exists in the database.
      *
-     * @param student
-     * the student to be added to database (via POST request)
-     *
-     * @throws IllegalStateException
-     * simple exception thrown for the sake of the course.
+     * @param student the student to be added to database (via POST request)
+     * @throws IllegalStateException simple exception thrown for the sake of the course.
      */
     private void ensureUniqueEmail(Student student) throws IllegalStateException {
         Optional<Student> queriedStudents = studentRepository.findStudentByEmail(student.getEmail());
-        if(queriedStudents.isPresent()) {
+        if (queriedStudents.isPresent()) {
             throw new IllegalStateException("Email is already taken.");
         }
+    }
+
+    private static NoSuchElementException produceNotExistsExceptionObject(Long studentId) {
+        return new NoSuchElementException(String.format(
+                "Student with id %d does not exist",
+                studentId
+        ));
     }
 
 }
